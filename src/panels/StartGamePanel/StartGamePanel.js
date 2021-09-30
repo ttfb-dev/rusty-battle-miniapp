873350ec1b6d5d250e3443c6945c1f5dc95f36c0
Orange <bobrov@ttfb.dev>
2021-09-30 16:34:25 +0300

    add start game example

--- src/panels/StartGamePanel/StartGamePanel.js
+++ src/panels/StartGamePanel/StartGamePanel.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { game } from 'store'
 
 import { Panel } from 'components/Panel'
 
 export const StartGamePanel = ({ id }) => {
+
+  const dispatch = useDispatch()
+
+  const startGame = () => {
+    dispatch.sync(game.action.startGame())
+  }
+
   return (
     <Panel id={id} header="StartGame">
       <h1>StartGame</h1>
