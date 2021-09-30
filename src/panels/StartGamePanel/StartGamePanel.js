bb3da1d004785f5cc9a0886942878c2eec0e4bc5
Orange <bobrov@ttfb.dev>
2021-09-30 16:37:31 +0300

    add action callbacks

--- src/panels/StartGamePanel/StartGamePanel.js
+++ src/panels/StartGamePanel/StartGamePanel.js
@@ -9,12 +9,18 @@ export const StartGamePanel = ({ id }) => {
   const dispatch = useDispatch()
 
   const startGame = () => {
-    dispatch.sync(game.action.startGame())
+    dispatch.sync(game.action.startGame()).then(() => {
+      console.log('then')
+    }).catch(() => {
+      console.log('catch')
+    }).finally(() => {
+      console.log('finally')
+    })
   }
 
   return (
     <Panel id={id} header="StartGame">
-      <h1>StartGame</h1>
+      <h1 onClick={startGame}>StartGame</h1>
     </Panel>
   )
 }
