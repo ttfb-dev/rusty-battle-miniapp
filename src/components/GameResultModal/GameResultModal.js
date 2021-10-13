b177e853593f31e3def884271d93cff8846802df
Orange <bobrov@ttfb.dev>
2021-10-13 15:13:17 +0300

    update backend

--- src/components/GameResultModal/GameResultModal.js
+++ src/components/GameResultModal/GameResultModal.js
@@ -15,7 +15,7 @@ import { EPanels } from 'constants/panels'
 import { game } from 'store'
 
 function declOfNum(number, titles) {  
-  cases = [2, 0, 1, 1, 1, 2];  
+  const cases = [2, 0, 1, 1, 1, 2];  
   return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];  
 }
 
--- src/components/RobotAvatar/RobotAvatar.js
+++ src/components/RobotAvatar/RobotAvatar.js
@@ -6,7 +6,7 @@ export const RobotAvatar = ({ slug, ...props }) => {
   return (
     <Avatar
       {...props}
-      src={`https://avatar.robots-game.ru/${md5(String(slug))}.png`}
+      src={`https://api.robots-game.ru/avatar/${md5(String(slug))}.png`}
     />
   )
 }
